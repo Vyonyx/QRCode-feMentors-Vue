c55412d25c3e29637061d26cd5df7d3c30f6caf7
personal <shaneel_kumar@live.com>
2023-04-08 22:08:59 +1200

    Initial commit & project finish

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Card from './components/Card.vue'
+</script>
+
+<template>
+  <Card title="Improve your front-end skills by building projects"
+    description="Scan the QR code to visit Frontend Mentor and take your coding skills to the next level"
+    image-url="/image-qr-code.png" />
+</template>
+
+<style scoped></style>
---
+++ src/components/Card.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+defineProps({
+  title: String,
+  description: String,
+  imageUrl: String,
+})
+</script>
+
+<template>
+  <div class="card">
+    <img :src="imageUrl" :alt="description">
+    <h3 class="title">{{ title }}</h3>
+    <p class="description">{{ description }}</p>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  font-family: 'Outfit', sans-serif;
+  max-width: 300px;
+  background: white;
+  padding: 1rem;
+  border-radius: 1rem;
+}
+
+.card img {
+  width: 100%;
+  aspect-ratio: 1/1;
+  border-radius: inherit;
+}
+
+.card .title {
+  color: hsl(218, 44%, 22%);
+  font-weight: 700;
+  font-size: 1.5rem;
+  font-family: inherit;
+  text-align: center;
+}
+
+.card .description {
+  color: hsl(220, 15%, 55%);
+  font-weight: 400;
+  font-size: 15px;
+  font-family: inherit;
+  text-align: center;
+  max-width: 85%;
+  margin: 0 auto;
+  margin-bottom: 2rem;
+}
+</style>
