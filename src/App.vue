1ad021b0fab5c50990c30e00db8d1ef941c1ea97
personal <shaneel_kumar@live.com>
2023-04-08 23:22:19 +1200

    import QR Code image into App component and rebuild to try fix asset issue in deployment

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import Card from './components/Card.vue'
+import qrCode from '/image-qr-code.png'
 </script>
 
 <template>
   <Card title="Improve your front-end skills by building projects"
     description="Scan the QR code to visit Frontend Mentor and take your coding skills to the next level"
-    image-url="/image-qr-code.png" />
+    :image-url="qrCode" />
 </template>
 
 <style scoped></style>
